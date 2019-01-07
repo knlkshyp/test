0aa9480edfa4037e4893ea8b52b1a672025ef68d
kunal-kashyap <kunal.kashhyap@techniatranscat.com>
2019-01-08 00:14:35 +0530

    First commit!

---
+++ script.js
@@ -0,0 +1,32 @@
+let btn = document.getElementById('btn');
+let msg = document.getElementById('msg');
+let det = document.getElementById('det');
+
+let toggle = false;
+
+btn.textContent = 'Show message';
+msg.style.visibility = 'hidden';
+det.setAttribute("value",getYmd());
+console.log();
+
+
+function getYmd() {
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth() + 1;
+    let date = new Date().getDate();
+    return `${year}-0${month}-0${date}`;
+}
+
+let click = () => {
+    toggle = !toggle;
+    if (toggle) {
+        msg.style.visibility = 'visible';
+        btn.textContent = 'Hide message';
+    } else {
+        msg.style.visibility = 'hidden';
+        btn.textContent = 'Show message';
+    }
+}
+
+btn.addEventListener('click', click);
+console.log();
